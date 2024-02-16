4331eeb133f84e6ae6f2a2872d2eeb479b236865
Andsirpa <andsirpa94@gmail.com>
2024-02-16 16:06:42 +0100

    added searchbar

--- src/App.vue
+++ src/App.vue
@@ -10,7 +10,23 @@ export default {
 </script>
 
 <!-- html -->
-<template></template>
+<template>
+  <header>
+    <div class="container mt-5">
+      <!-- creo la searchbar -->
+      <nav class="navbar bg-body-tertiary">
+        <div class="container-fluid">
+          <a class="navbar-brand">Boolflix</a>
+          <form class="d-flex" role="search">
+            <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+            <button class="btn btn-primary btn-outline-success" type="submit">Search</button>
+          </form>
+        </div>
+      </nav>
+    </div>
+
+  </header>
+</template>
 
 <!-- css -->
 <style lang ="scss">
