7b916b3b47618010cf5a3fa2594476242d09b50d
Andsirpa <andsirpa94@gmail.com>
2024-02-16 17:17:40 +0100

    feat: added method Search

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,34 @@
 <!-- js -->
 <script>
+import axios from "axios";
 import { store } from "./store";
 
 export default {
   data() {
     return {
       store,
+      searchedTerm: '',
 
     };
+  },
+
+  // creo il metodo per la ricerca
+  methods: {
+    Search() {
+      axios
+        .get(this.store.api.uri + '/search/movie', {
+          // in params posso mettere tanti parametri
+          params: {
+            // recupero la key e la query
+            api_key: store.api.key,
+            query: this.searchedTerm,
+          },
+        })
+        .then((response) => {
+          console.log(response.data);
+        });
+    },
+
   },
 };
 </script>
@@ -21,8 +42,9 @@ export default {
         <div class="container-fluid">
           <a class="navbar-brand">Boolflix</a>
           <form class="d-flex" role="search">
-            <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-            <button class="btn btn-primary btn-outline-success" type="submit">Search</button>
+            <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"
+              v-model="searchedTerm">
+            <button class="btn btn-primary btn-outline-success" type="submit" @click="Search()">Search</button>
           </form>
         </div>
       </nav>
--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,10 @@
 import { reactive } from "vue";
 
 export const store = reactive({
-    apiUri: '',
-    apiKey: '',
+    // raggruppi le api in un unico oggetto
+    api: {
+        uri: 'https://api.themoviedb.org/3/',
+        // salvo la mia chiave
+        key: 'db0595e17eecf660fba86c3530dedbff',
+    }
 })
