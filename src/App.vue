9fd81848303e7f8b1b600521729004cd07801c91
Andsirpa <andsirpa94@gmail.com>
2024-02-20 14:27:03 +0100

    feat: take only chosen keys using map

--- src/App.vue
+++ src/App.vue
@@ -25,7 +25,18 @@ export default {
           },
         })
         .then((response) => {
-          store.movies = response.data.results;
+          store.movies = response.data.results.map((movie) => {
+            // uso il map per creare una copia con solo le informazioni che utilizzo
+            return {
+              // nel map posso modificare i nomi chiavi
+              title: movie.title,
+              original_title: movie.original_title,
+              language: movie.original_language,
+              vote: movie.vote_average,
+
+
+            }
+          });
 
         });
     },
@@ -39,7 +50,6 @@ export default {
   <header>
     <div class="container mt-5">
       <!-- creo la searchbar -->
-
       <div class="d-flex">
         <input class="form-control me-2" type="text" placeholder="Search" v-model="searchedTerm" @keyup.enter="Search()">
         <button class="btn btn-primary" @click="Search()">Search</button>
@@ -51,11 +61,11 @@ export default {
       <div class="container mt-5">
         <!-- info che mi servono -->
         <ul v-for="movie in store.movies">
-          <!-- prendo i nomi delle chiavi che voglio -->
+          <!-- prendo i nomi delle chiavi che voglio usare -->
           <li>titolo: {{ movie.title }}</li>
           <li>titolo originale: {{ movie.original_title }}</li>
-          <li>lingua: {{ movie.original_language }}</li>
-          <li>voto: {{ movie.vote_average }}</li>
+          <li>lingua: {{ movie.language }}</li>
+          <li>voto: {{ movie.vote }}</li>
         </ul>
       </div>
 
