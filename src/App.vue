0bb3cfb2acf6a86c5fa75add25e462aaba97ca7e
Andsirpa <andsirpa94@gmail.com>
2024-02-20 20:47:19 +0100

    add method fetchTvSeries (using destructuring)

--- src/App.vue
+++ src/App.vue
@@ -26,8 +26,8 @@ export default {
           },
         })
         .then((response) => {
+          // uso il map per creare una copia con solo le informazioni che utilizzo
           store.movies = response.data.results.map((movie) => {
-            // uso il map per creare una copia con solo le informazioni che utilizzo
             return {
               // nel map posso modificare i nomi chiavi
               title: movie.title,
@@ -40,24 +40,48 @@ export default {
 
     },
 
+    fetchTvSeries() {
+      axios
+        .get(`${this.store.api.uri}/search/tv`, {
+          // in params posso mettere tanti parametri
+          params: {
+            // recupero la key e la query
+            api_key: store.api.key,
+            query: this.searchedTerm,
+          },
+        })
+        .then((response) => {
+          // qui uso il destructuring, sintassi alternativa
+          store.tvSeries = response.data.results.map((tvSerie) => {
+            // prendo le proprietà dall'oggetto tvSerie..
+            const { title, original_title, language, vote } = tvSerie;
+            // ..e le assegno al nuovo oggetto che diventa l'elemento del nuovo array tvSeries messo in store
+            return { title, original_title, language, vote };
+          });
+        });
+
+    },
+
+  }
     Search() {
-      this.fetchMovies();
-    },
+    this.fetchMovies();
+    this.fetchTvSeries();
+  },
 
 
-    // aggiungo le bandiere
+  // aggiungo le bandiere
 
-    getFlag(langCode) {
-      // guard close per rendere il codice più compatto
-      if (langCode == 'it') return new URL('./assets/img/itflag.jpeg', import.meta.url).href;
-      if (langCode == 'en') return new URL('./assets/img/engflag.png', import.meta.url).href;
+  getFlag(langCode) {
+    // guard close per rendere il codice più compatto
+    if (langCode == 'it') return new URL('./assets/img/itflag.jpeg', import.meta.url).href;
+    if (langCode == 'en') return new URL('./assets/img/engflag.png', import.meta.url).href;
 
-      // se la lingua è diversa metto una bandiera bianca
-      return new URL('./assets/img/whiteflag.jpeg', import.meta.url).href;
-
-    },
+    // se la lingua è diversa metto una bandiera bianca
+    return new URL('./assets/img/whiteflag.jpeg', import.meta.url).href;
 
   },
+
+},
 };
 </script>
 
